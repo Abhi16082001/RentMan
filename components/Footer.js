19bc9ef5ff98e3860492a50d574e44f020579971
Abhishek Rajput <abhishekrajput16082001@gmail.com>
2024-11-17 16:11:35 +0530

    Added Favicon and update Footer !

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
 
   return (
     <div className=' bg-violet-500 p-3 text-center '>
-     <span className='font-semibold text-center'> R∉nt Man is a Next App for Management of Renters in Apartments or Buildings. </span>
+     <span className='font-semibold text-center'> R∉nt Man is a Next App for Management of Renters in Apartments or Buildings - ABHISHEK RAJPUT. </span>
 
       
     </div>
